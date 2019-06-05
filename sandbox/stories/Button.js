9eea94f2c7be2f690b47aec69c181216f7c729f8
Tuan Mai <perhuman@gmail.com>
2019-06-05 11:32:40 +0700

    Add flow and eslint Fix some errors after add eslint

--- sandbox/stories/Button.js
+++ sandbox/stories/Button.js
@@ -2,18 +2,6 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import Button from '@hero-design/lib/src/components/Button';
 
-class ButtonScreen extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.pusher} />
-        <Button color="red">Button</Button>
-        <Button bottom>Button</Button>
-      </View>
-    );
-  }
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -24,5 +12,13 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+
+const ButtonScreen = () => (
+  <View style={styles.container}>
+    <View style={styles.pusher} />
+    <Button color="red">Button</Button>
+    <Button bottom>Button</Button>
+  </View>
+);
 
 export default ButtonScreen;
--- sandbox/stories/TextInput.js
+++ sandbox/stories/TextInput.js
@@ -2,23 +2,19 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import TextInput from '@hero-design/lib/src/components/TextInput';
 
-class TextInputScreen extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <TextInput />
-        <TextInput error="Email can not be blank" />
-        <TextInput placeholder="Enter your phone number" keyboard="numeric" />
-      </View>
-    );
-  }
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
   },
 });
+
+const TextInputScreen = () => (
+  <View style={styles.container}>
+    <TextInput />
+    <TextInput error="Email can not be blank" />
+    <TextInput placeholder="Enter your phone number" keyboard="numeric" />
+  </View>
+);
 
 export default TextInputScreen;
