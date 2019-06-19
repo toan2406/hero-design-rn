bde152813cd613d4a8dd85613cb518b21b8792b9
Tuan Mai <perhuman@gmail.com>
2019-06-19 15:51:30 +0700

    Add support fontWeight for Text component

--- sandbox/stories/Text.js
+++ sandbox/stories/Text.js
@@ -13,12 +13,51 @@ class TextScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Text> Normal </Text>
         <Text h1> Heading 1 </Text>
         <Text h2> Heading 2 </Text>
         <Text h3> Heading 3 </Text>
         <Text h4> Heading 4 </Text>
         <Text h5> Heading 5 </Text>
         <Text h6> Heading 6 </Text>
+        <Text> Bold </Text>
+        <Text h1 bold>
+          Heading 1
+        </Text>
+        <Text h2 bold>
+          Heading 2
+        </Text>
+        <Text h3 bold>
+          Heading 3
+        </Text>
+        <Text h4 bold>
+          Heading 4
+        </Text>
+        <Text h5 bold>
+          Heading 5
+        </Text>
+        <Text h6 bold>
+          Heading 6
+        </Text>
+        <Text> Light </Text>
+        <Text h1 light>
+          Heading 1
+        </Text>
+        <Text h2 light>
+          Heading 2
+        </Text>
+        <Text h3 light>
+          Heading 3
+        </Text>
+        <Text h4 light>
+          Heading 4
+        </Text>
+        <Text h5 light>
+          Heading 5
+        </Text>
+        <Text h6 light>
+          Heading 6
+        </Text>
       </View>
     );
   }
