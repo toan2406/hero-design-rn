e5730be36ce8bcf5169a0c0f35dddf09ea21d9b4
Tuan Mai <perhuman@gmail.com>
2019-07-02 15:28:34 +0700

    Customize style follow https://everydayhero.github.io/constructicon/

---
+++ documentation/docs/installation.md
@@ -0,0 +1,21 @@
+### Using yarn
+```bash
+yarn add hero-design-rn
+
+```
+
+### Using npm
+```bash
+npm install hero-design-rn
+```
+
+
+### Using components
+```md
+import { Button } from 'hero-design-rn';
+
+const App = () => (
+  <Button children="Click me" color="red" bottom onPress={console.log} />
+)
+
+```
--- documentation/styleguide.config.js
+++ documentation/styleguide.config.js
@@ -1,8 +1,42 @@
 const path = require("path");
+
 const libPath = path.dirname(require.resolve("../lib/package.json"));
+const { styles, theme } = require("./styleguide.style");
 
 module.exports = {
-  components: `${libPath}/src/components/**/*.{js,jsx}`,
+  styles,
+  theme,
+  sections: [
+    {
+      name: "Introduction",
+      content: "docs/introduction.md"
+    },
+    {
+      name: "Documentation",
+      sections: [
+        {
+          name: "Installation",
+          content: "docs/installation.md",
+          description: "The description for the installation section"
+        },
+        {
+          name: "Customization",
+          content: "docs/customization.md"
+        },
+        {
+          name: "Theme configuration",
+          components: `../lib/src/config/theme.js`,
+          content: "docs/theme.md"
+        }
+      ]
+    },
+    {
+      name: "Components",
+      components: `${libPath}/src/components/**/*.{js,jsx}`,
+      exampleMode: "collapse",
+      usageMode: "expand"
+    }
+  ],
   webpackConfig: {
     module: {
       rules: [
@@ -13,7 +47,5 @@ module.exports = {
         }
       ]
     }
-  },
-  exampleMode: "collapse",
-  usageMode: "expand"
+  }
 };
